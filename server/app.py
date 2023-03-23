542c87781c1743d58713e98a5dfdbd6237918910
Robert Gorz <rjgorz@gmail.com>
2023-03-22 19:18:49 -0500

    models and app populated but post wont pass tests'

--- server/app.py
+++ server/app.py
@@ -17,14 +17,78 @@ db.init_app(app)
 
 api = Api(app)
 
-@app.route('/')
-def index():
-    return '<h1>Code challenge</h1>'
+class Restaurants(Resource):
+    def get(self):
+        restaurant_dicts = [restaurant.to_dict() for restaurant in Restaurant.query.all()]
 
-@app.route('/restaurants')
-def restaurants():
+        return make_response(
+            restaurant_dicts,
+            200
+        )
+    
+api.add_resource(Restaurants, '/restaurants')
 
-    pass
+
+class RestaurantById(Resource):
+    def get(self, id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+
+        if not restaurant:
+            return make_response({ "error": "Restaurant not found"}, 404)
+        
+        return make_response(
+            restaurant.to_dict(rules=('pizzas',)),
+            200
+        )
+    
+    def delete(self, id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+
+        if not restaurant:
+            return make_response({ "error": "Restaurant not found"}, 404)
+        
+        restaurant_pizzas = RestaurantPizza.query.filter_by(restaurant_id=id).all()
+        for restaurant_pizza in restaurant_pizzas:
+            db.session.delete(restaurant_pizza)
+        db.session.delete(restaurant)
+        db.session.commit()
+
+        return make_response({ "success": "" }, 200)
+    
+api.add_resource(RestaurantById, '/restaurants/<int:id>')
+
+class Pizzas(Resource):
+    def get(self):
+        pizza_dicts = [pizza.to_dict() for pizza in Pizza.query.all()]
+
+        return make_response(
+            pizza_dicts,
+            200
+        )
+    
+api.add_resource(Pizzas, '/pizzas')
+
+class RestaurantPizzas(Resource):
+    def post(self):
+        try:
+            new_restaurantPizza = RestaurantPizza(
+                price=request.get_json()['price'],
+                pizza_id=request.get_json()['pizza_id'],
+                restaurant_id=request.get_json()['restaurant_id']
+            )
+
+            db.session.add(new_restaurantPizza)
+            db.session.commit()
+
+            return make_response(
+                new_restaurantPizza.pizza.to_dict(),
+                201
+            )
+        
+        except ValueError:
+            return make_response({ "error": "Invalid input" }, 400)
+        
+api.add_resource(RestaurantPizzas, '/restaurant_pizzas')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -11,3 +11,50 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = 'restaurants'
+
+    serialize_rules = ('-restaurant_pizzas', '-pizzas.restaurants')
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='restaurant')
+    pizzas = association_proxy('restaurant_pizzas', 'pizza')
+
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = 'pizzas'
+
+    serialize_rules = ('-restaurant_pizzas', '-restaurants.pizzas')
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza')
+    restaurants = association_proxy('restaurant_pizzas', 'restaurant')
+
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = 'restaurant_pizzas'
+
+    serialize_rules = ('-pizza.restaurant_pizzas', '-pizza.restaurants',
+                       '-restaurant.restaurant_pizzas', '-restaurant.pizzas')
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+
+    @validates('price')
+    def validate_price(self, key, price):
+        if price < 1 or price > 30:
+            raise ValueError('Invalid input')
+        return price
